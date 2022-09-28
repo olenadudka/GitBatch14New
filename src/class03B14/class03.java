7d7a9c3c174cad78bdb3afad6acc8473b5d115a6
olenadudka <helen.lady2311@gmail.com>
2022-09-27 21:34:16 -0400

    First commit

---
+++ src/class03B14/class03.java
@@ -0,0 +1,12 @@
+package class03B14;
+
+public class class03 {
+
+	public static void main(String[] args) {
+		System.out.println("hey this is a git class");
+System.out.println("I made a change in my project");
+System.out.println("One more message");
+System.out.println("How are you this a new version");
+	}
+
+}
